3438ae3fc3d94ab23378969f0df834d44be3fc96
davis <dbh937@gmail.com>
2015-04-16 18:21:27 -0400

    made the angular.js file a static file so it doesn't interfere with django templates

--- dash_demo/urls.py
+++ dash_demo/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from dashboard import views
 urlpatterns = [
     # Examples:
     # url(r'^$', 'dash_demo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^dashboard/', include('dashboard.urls')),
+    # url(r'^$', 'dashboard.views.index', name='home')
 ]
+
+from . import settings
+if settings.DEBUG:
+    urlpatterns += url(
+        r'^$', 'django.contrib.staticfiles.views.serve', kwargs={
+            'path': 'index.html'}),
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 # Create your views here.
+
+
+def index(request):
+    # template = loader.get_template('dashboard/index.html')
+    # return HttpResponse(template.render())
+    return render_to_response('dashboard/static/app/dashboard/index.html')
