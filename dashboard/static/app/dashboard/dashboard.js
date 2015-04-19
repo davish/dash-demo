97c81c44fed86d7fd03a81482a3e0308afa0e175
davis <dbh937@gmail.com>
2015-04-19 01:36:19 -0400

    added cumulative growth chart

--- dashboard/static/app/dashboard/dashboard.js
+++ dashboard/static/app/dashboard/dashboard.js
@@ -1,5 +1,9 @@
 /**
  * Created by davis on 4/16/15.
+ *
+ * TODO: Datepicker for range
+ * TODO: "Zoom Out" button
+ * TODO: Map queries to URLs for navigation
  */
 var dashControllers = angular.module('dashControllers', ['chart.js']);
 
@@ -8,6 +12,17 @@ n = -1;
 dashControllers.controller('dashTestCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.barseries = ['Rep Signups', 'Non-Rep Signups'];
     $scope.pielabels = ['Rep Signups', 'Non-Rep Signups'];
+    $scope.differentiate = true;
+    $scope.cumulative = false;
+    $scope.toggle = function(b) {
+        $scope[b] = !$scope[b];
+        $scope.getData();
+    };
+    $scope.toggleCumulative
+    $scope.opts = {
+        tooltipTemplate: " <%=label%>: <%= numeral(value).format('(00[.]00)') %> " +
+        "- <%= numeral(circumference / 6.283).format('(0[.][00]%)') %>"
+    };
     $scope.onClick = function(points, evt) {
         var start = null, end = null;
         if (n <= 14) { // currently displaying by day; can't zoom in any further.
@@ -48,9 +63,29 @@ dashControllers.controller('dashTestCtrl', ['$scope', '$http', function($scope, 
             });
 
             var r = generateChart(data);
-
+            var bardata;
+            var barseries;
             $scope.barlabels = r['dates'];
-            $scope.bardata = r['stats'];
+            if ($scope.differentiate) {
+                bardata = r['stats'].slice(0,2);
+                barseries = ['Rep Signups', 'Non-Rep Signups'];
+            } else {
+                bardata = [r['stats'][2]];
+                barseries = ['Signups'];
+            }
+            if ($scope.cumulative) { // if we wanna see the cumulative growth
+                var cumul = [];
+                for (var j=0; j<bardata.length;j++) {
+                    var c = [bardata[j][0]];
+                    for (var i=1; i<bardata[j].length;i++) {
+                        c.push(c[c.length-1]+bardata[j][i]);
+                    }
+                    cumul.push(c);
+                }
+                bardata = cumul;
+            }
+            $scope.bardata = bardata;
+            $scope.barseries = barseries;
             $scope.mode = r['mode'];
             if (r['stats'][0] && r['stats'][1])
                 $scope.piedata = [r['stats'][0].reduce(function(x,y){return x+y}), r['stats'][1].reduce(function(x,y){return x+y})];
@@ -61,7 +96,7 @@ dashControllers.controller('dashTestCtrl', ['$scope', '$http', function($scope, 
     $scope.getData();
 }]);
 month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-day_names = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+day_names = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
 // year is normal, but month is 0-based.
 function daysInMonth(d) {
     d = new Date(d); // make sure it's not a datestring, but an actual date object.
@@ -70,14 +105,13 @@ function daysInMonth(d) {
 
 function generateChart(data) {
     var dates = [];
-    var stats = [[], []];
+    var stats = [[], [], []];
     var mode = '';
     /*
     * if the length is less than 14, just display it by day.
     * if the length is between 15 and 56, add it up by week.
     * greater than 56, do it by month.
     * greater than two years, do it by year.
-    * TODO: refactor into one for loop for compactness.
     * */
     n = data.length;
     if (data.length <= 14) {
@@ -90,6 +124,7 @@ function generateChart(data) {
             dates.push(day_names[fdate.getUTCDay()]+ ' ' + (fdate.getUTCMonth()+1) +'/'+  fdate.getUTCDate());
             stats[0].push(element['day_rep_signups']);
             stats[1].push(element['day_nonrep_signups']);
+            stats[2].push(element['day_rep_signups']+element['day_nonrep_signups']);
         }
     }
     /*
@@ -112,6 +147,8 @@ function generateChart(data) {
                     dates.push((fdate.getUTCMonth()+1) +'/'+  fdate.getUTCDate() +'/'+ fdate.getUTCFullYear()%100);
                     stats[0].push(week[0]);
                     stats[1].push(week[1]);
+                    stats[2].push(week[0]+week[1]);
+
                     week = [0,0];
                 }
             }
@@ -119,6 +156,7 @@ function generateChart(data) {
         // push the remaining data.
         stats[0].push(week[0]);
         stats[1].push(week[1]);
+        stats[2].push(week[0]+week[1]);
     }
     /*
      * This is harder, since months have variable length, and some days might be missing.
@@ -141,6 +179,7 @@ function generateChart(data) {
                 var lastMonth = new Date(date.getUTCFullYear(), date.getUTCMonth()-1);
                 stats[0].push(month[0]);
                 stats[1].push(month[1]);
+                stats[2].push(month[0]+month[1]);
                 dates.push(month_names[lastMonth.getUTCMonth()] + ' ' + date.getFullYear());
                 // start over
                 month[0] = parseInt(element['day_rep_signups']);
@@ -151,7 +190,9 @@ function generateChart(data) {
         dates.push(month_names[new Date(data[i-1]['fields'].date).getUTCMonth()] + ' ' + date.getFullYear());
         stats[0].push(month[0]);
         stats[1].push(month[1]);
+        stats[2].push(month[0]+month[1]);
     }
+
     return {dates: dates, stats: stats, mode: mode};
 }
 
