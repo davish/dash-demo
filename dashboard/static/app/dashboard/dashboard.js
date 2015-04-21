be16a7d249fc57af09e011ec085bc0123cf944f7
davis <dbh937@gmail.com>
2015-04-20 22:34:14 -0400

    cumulative WoW stats and path implemented.

--- dashboard/static/app/dashboard/dashboard.js
+++ dashboard/static/app/dashboard/dashboard.js
@@ -1,51 +1,89 @@
 /**
  * Created by davis on 4/16/15.
  *
- * TODO: Datepicker for range
- * TODO: "Zoom Out" button
- * TODO: Map queries to URLs for navigation
+ * DONE: Datepicker for range
+ * DONE: "Zoom Out" button TODO: Make zoom out better
+ * DONE: Map queries to URLs for navigation
+ * DONE: WoW growth instead of just cumulative signups
+ * DONE: Fix week counting, it's a problem.
+ * DONE: Make WoW growth use aggregate fields from statistics to save on Math and make it not change based on startdate
+ * TODO: save the chart(s) to a PDF
  */
 var dashControllers = angular.module('dashControllers', ['chart.js']);
 
-
-
 n = -1;
 
-dashControllers.controller('dashTestCtrl', ['$scope', '$http', function($scope, $http) {
-    $scope.barseries = ['Rep Signups', 'Non-Rep Signups'];
-    $scope.pielabels = ['Rep Signups', 'Non-Rep Signups'];
+dashControllers.controller('dashTestCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
     $scope.differentiate = true;
     $scope.cumulative = false;
     $scope.toggle = function(b) {
         $scope[b] = !$scope[b];
-        $scope.getData();
+        $scope.refreshGraphs();
+
+        if (b === 'cumulative') {
+            if ($scope.cumulative) {
+                $scope.line.options.scaleLabel = "<%= value %> %";
+                //$scope.line.options.scaleOverride = true;
+                $scope.line.options.multiTooltipTemplate = '<%=datasetLabel%>: <%= numeral(value).format(\'0.00\') %>%';
+                $scope.line.options.tooltipTemplate = "<%=label%>: <%=numeral(value).format('0.00')%>%";
+            } else {
+                $scope.line.options.scaleLabel = undefined;
+                //$scope.line.options.scaleOverride = false;
+                $scope.line.options.multiTooltipTemplate = '<%=datasetLabel%>: <%= numeral(value).format(\'0,0\') %>';
+                $scope.line.options.tooltipTemplate = '<%=label%>: <%=numeral(value).format(\'0,0\')%>'
+            }
+        }
     };
 
-    $scope.opts = {
-        tooltipTemplate: " <%=label%>: <%= numeral(value).format('(00[.]00)') %> " +
-        "- <%= numeral(circumference / 6.283).format('(0[.][00]%)') %>"
-    };
-    $scope.onClick = function(points, evt) {
-        var start = null, end = null;
-        if (n > 14)  { // zoom into a day view
-            var match = points[0].label.match(/(\d+)\/(\d+)\/(\d+)/);
-            var date = new Date(2000+parseInt(match[3]), parseInt(match[1])-1, match[2]);
-            start = date;
-            end = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 6);
+    $scope.line = {
+        data: [],
+        series: [],
+        labels: [],
+        options: {
+            bezierCurve : false,
+            pointHitDetectionRadius : 1,
+            multiTooltipTemplate: '<%=datasetLabel%>: <%= numeral(value).format(\'0,0\') %>',
+            tooltipTemplate: '<%=label%>: <%=numeral(value).format(\'0,0\')%>',
+            scaleOverride: false,
+            scaleStartValue: 0,
+            scaleStepWidth: 1,
+            scaleSteps: 100,
+            scaleBeginAtZero: true
+        },
+        onClick: function(points, evt) {
+            var start = null, end = null;
+            if ($scope.mode == 1)  { // zoom into a day view
+                var match = points[0].label.match(/(\d+)\/(\d+)\/(\d+)/);
+                var date = new Date(2000+parseInt(match[3]), parseInt(match[1])-1, match[2]);
+                start = date;
+                end = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 6);
+                $scope.mode = 0;
+            }
+            if (start && end && start < end) {
+                $scope.startDate = start.toISOString().slice(0, 10);
+                $scope.endDate = end.toISOString().slice(0, 10);
+                $scope.refreshGraphs();
+            }
         }
-        if (start && end && start < end) {
-            $scope.startDate = start.toISOString().slice(0, 10);
-            $scope.endDate = end.toISOString().slice(0, 10);
-            $scope.getData();
+    };
+
+    $scope.pie = {
+        data: [],
+        labels: ["Rep Signups", "Non-Rep Signups"],
+        options: {
+            animation: false,
+            tooltipTemplate: " <%=label%>: <%= numeral(circumference / 6.283).format('(0[.][00]%)') %> " +
+            "(<%= numeral(value).format('0,0') %>)"
         }
     };
+
     $scope.zoomOut = function() {
         var startZoom = new Date($scope.startDate);
         var endZoom = new Date(startZoom.getUTCFullYear(), startZoom.getUTCMonth(),
             startZoom.getUTCDate() + daysInMonth(startZoom)).toISOString().slice(0,10);
         console.log(endZoom);
         $scope.endDate = endZoom;
-        $scope.getData();
+        $scope.refreshGraphs();
     };
     // by default, start with this year so far.
     var end = new Date();
@@ -54,45 +92,44 @@ dashControllers.controller('dashTestCtrl', ['$scope', '$http', function($scope, 
     $scope.startDate = start.toISOString().slice(0,10);
     $scope.endDate = end.toISOString().slice(0,10);
     // sample data ?method=range&start=2014-9-2&end=2015-9-15
-    $scope.getData = function() {
+
+    $scope.$watch(function(scope) { return scope.startDate }, function(n, o) {
+        $location.path($scope.startDate + '/' + $scope.endDate);
+        $scope.refreshGraphs();
+    });
+    $scope.$watch(function(scope) { return scope.endDate }, function(n, o) {
+        $location.path($scope.startDate + '/' + $scope.endDate);
+        $scope.refreshGraphs();
+    });
+    $scope.$watch(function() {return $location.path()}, function(n, o) {
+        var r = n.match(/\/([0-9\-]+)\/([0-9\-]+)/)
+        $scope.startDate = r[1];
+        $scope.endDate = r[2];
+    });
+
+    $scope.refreshGraphs = function() {
         $http.get('/dashboard/api?method=range&start='+$scope.startDate+'&end='+$scope.endDate).success(function(d) {
             var data = JSON.parse(d.data);
-            data = data.sort(function(a, b) {
-                return new Date(a['fields'].date) - new Date(b['fields'].date);
-            });
-
-            var r = generateChart(data);
-            var bardata;
-            var barseries;
-            $scope.barlabels = r['dates'];
-            if ($scope.differentiate) { // if we're splitting up rep/non-rep signups
-                bardata = r['stats'].slice(0,2);
-                barseries = ['Rep Signups', 'Non-Rep Signups'];
-            } else {
-                bardata = [r['stats'][2]];
-                barseries = ['Signups'];
-            }
-            if ($scope.cumulative) { // if we wanna see the cumulative growth
-                var cumul = [];
-                for (var j=0; j<bardata.length;j++) {
-                    var c = [bardata[j][0]];
-                    for (var i=1; i<bardata[j].length;i++) {
-                        c.push(c[c.length-1]+bardata[j][i]);
-                    }
-                    cumul.push(c);
-                }
-                bardata = cumul;
-            }
-            $scope.bardata = bardata;
-            $scope.barseries = barseries;
-            $scope.mode = r['mode'];
-            if (r['stats'][0] && r['stats'][1])
-                $scope.piedata = [r['stats'][0].reduce(function(x,y){return x+y}), r['stats'][1].reduce(function(x,y){return x+y})];
-            else
-                $scope.piedata = [];
+            $scope.rawData = data;
+            $scope.redrawGraphs(data);
         });
     };
-    $scope.getData();
+    $scope.redrawGraphs = function(data) {
+        data = data.sort(function(a, b) {
+            return new Date(a['fields'].date) - new Date(b['fields'].date);
+        });
+
+        var r = genChart(data, $scope.cumulative, $scope.mode);
+        $scope.line.series=$scope.differentiate ? ['Rep Signups', 'Non-Rep Signups'] : ['Signups'];
+        $scope.line.data = $scope.differentiate ? r['stats'].slice(0,2) : [r['stats'][2]];
+        $scope.line.labels = r['dates'];
+        $scope.mode = r['mode'];
+        if (r['stats'][0] && r['stats'][1])
+            $scope.pie.data = [r['stats'][0].reduce(function(x,y){return x+y}), r['stats'][1].reduce(function(x,y){return x+y})];
+        else
+            $scope.pie.data = [];
+    };
+    $scope.refreshGraphs();
 }]);
 month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 day_names = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
@@ -102,48 +139,48 @@ function daysInMonth(d) {
     return new Date(d.getFullYear(), d.getUTCMonth()+1, 0).getDate();
 }
 
-function generateChart(data) {
+function genChart(data, cumulative, mode) {
     var dates = [];
     var stats = [[], [], []];
-    var mode = '';
-    /*
-    * if the length is less than 14, just display it by day.
-    * if the length is between 15 and 56, add it up by week.
-    * greater than 56, do it by month.
-    * greater than two years, do it by year.
-    * */
-    n = data.length;
-    if (data.length <= 14) {
-        mode = 0;
+    if (data.length < 14) mode = 0;
+    if (mode == 0) {
         for (var i in data) {
             var element = data[i]['fields'];
             var fdate = new Date(element.date);
-            // for some reason, using local timezone gives the wrong date (day earlier in this case.
-            // TODO: Check in the morning if this still gives the right date.
             dates.push(day_names[fdate.getUTCDay()]+ ' ' + (fdate.getUTCMonth()+1) +'/'+  fdate.getUTCDate());
-            stats[0].push(element['day_rep_signups']);
-            stats[1].push(element['day_nonrep_signups']);
-            stats[2].push(element['day_rep_signups']+element['day_nonrep_signups']);
+            var r = cumulative ? cumulativeElement(element) : absoluteElement(element);
+            stats[0].push(r[0]);
+            stats[1].push(r[1]);
+            stats[2].push(r[2]);
         }
     }
     /*
      * Have to remember that some data might not be there. if (data.length <=56)
      */
     else { // max is eight weeks. (should it be 12 weeks?)
-        mode = 1;
         var i=0;
-        var week = [0,0];
+        var week = [0,0,0];
         var fdate = new Date(new Date(data[i]['fields'].date));
         dates.push((fdate.getUTCMonth()+1) +'/'+  fdate.getUTCDate() +'/'+ fdate.getUTCFullYear()%100);
         for (i; i < data.length; i++) {
             var element = data[i]['fields'];
-            var date = new Date(element.date);
+
             week[0] += parseInt(element['day_rep_signups']);
             week[1] += parseInt(element['day_nonrep_signups']);
-            if (date.getDay() == 0) {
+
+            var date = new Date(element.date);
+            if (date.getUTCDay() == 0) {
                 if (i != 0) { // if this isn't the first thing in the array,
-                    var fdate = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate())
+                    var fdate = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
                     dates.push((fdate.getUTCMonth()+1) +'/'+  fdate.getUTCDate() +'/'+ fdate.getUTCFullYear()%100);
+                    week[2] = week[0] + week[1];
+                    /*
+                     * WoW growth.
+                     * given set W and week n, WoW growth is
+                     * W[n] / sum(W[n-1])
+                     */
+                    if (cumulative) cumulativeWeek(week, element);
+
                     stats[0].push(week[0]);
                     stats[1].push(week[1]);
                     stats[2].push(week[0]+week[1]);
@@ -152,7 +189,9 @@ function generateChart(data) {
                 }
             }
         }
+        week[2] = week[0] + week[1];
         // push the remaining data.
+        if (cumulative) cumulativeWeek(week, data[i-1]['fields']);
         stats[0].push(week[0]);
         stats[1].push(week[1]);
         stats[2].push(week[0]+week[1]);
@@ -161,6 +200,27 @@ function generateChart(data) {
     return {dates: dates, stats: stats, mode: mode};
 }
 
+function absoluteElement(element) {
+    return [
+        element['day_rep_signups'],
+        element['day_nonrep_signups'],
+        element['day_rep_signups']+element['day_nonrep_signups']
+    ];
+}
+
+function cumulativeElement(e) {
+    return [
+        e['day_rep_signups'] / (e['aggregate_rep_signups']-e['day_rep_signups'])*100,
+        e['day_nonrep_signups'] / ((e['aggregate_total_signups'] - e['aggregate_rep_signups']) - e['day_nonrep-signups'])*100,
+        5
+    ]
+}
+
+function cumulativeWeek(week, latestElement) {
+    week[0] = week[0] / (latestElement['aggregate_rep_signups']-week[0])*100;
+    week[1] = week[1] / ((latestElement['aggregate_total_signups'] - latestElement['aggregate_rep_signups']) - week[1])*100;
+    week[2] = week[2] / (latestElement['aggregate_total_signups'] - week[2])*100;
+}
 
 angular.module('dashFilters', []).filter('unit', function() {
     return function(input) {
